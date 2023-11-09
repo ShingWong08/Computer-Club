5d9dfaccd6973a2802614f884e9509f0fbd696be
ShingWong08 <103511887+ShingWong08@users.noreply.github.com>
2023-11-09 17:27:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+        // Declare Variable
+        int NumberA = 4;
+        int NumberB = 5;
+
+        // Change The Variable From Integer To String
+        String a = String.valueOf(NumberA);
+        String b = String.valueOf(NumberB);
+
+        System.out.println(a + b);
+
+        for (int i = 0; i <= 81; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
